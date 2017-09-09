9967e1e2cd014f79e06f0eb0e069cecf197b48ae
Max Muth <kontakt@maxi-muth.de>
2017-09-09 11:29:55 +0200

    Add status and history routes

--- Neitha-Server.py
+++ Neitha-Server.py
@@ -4,29 +4,20 @@ from flask import Flask, request, json
 app = Flask(__name__)
 
 
-# def get_last_connected():
-#     last_connected = getattr(g, 'last_connected', 0)
-#     return last_connected
-#
-# def update_last_connected():
-#     with app.app_context():
-#         # within this block, current_app points to app.
-#         print
-#         current_app.name
-
 state_history = []
 
 
 def serialize_timestamp(datetime=datetime.now()):
+    """ Takes a datetime object and serializes it to our """
     return datetime.strftime("%Y-%m-%d %H:%M:%S")
 
 
 @app.route('/ping', methods=['GET', 'POST'])
 def ping():
-    # last_connected = datetime.now()
     longitude = request.args.get('longitude')
     latitude = request.args.get('latitude')
     connected = request.args.get('connected')
+
     if None in [longitude, latitude, connected]:
         response = app.response_class(
             response='Bad request: we need the following URL parameters: longitude, latitude, connected',
@@ -34,6 +25,7 @@ def ping():
             mimetype='application/json'
         )
         return response
+
     updated_data = {
         'connected': connected,
         'longitude': longitude,
@@ -41,7 +33,7 @@ def ping():
         'timestamp': serialize_timestamp()
     }
     state_history.append(updated_data)
-    data = {
+    response_data = {
         'message': 'Updated.',
         'connected': connected,
         'longitude': longitude,
@@ -49,14 +41,50 @@ def ping():
         'history': state_history
     }
     response = app.response_class(
-        response=json.dumps(data),
+        response=json.dumps(response_data),
         status=200,
         mimetype='application/json'
     )
     return response
 
-# @app.route('/get_status', methods=['GET'])
-# def ping():
+
+@app.route('/status', methods=['GET'])
+def get_status():
+    if len(state_history) == 0:
+        response = app.response_class(
+            response='We have no reported status yet',
+            status=400,
+            mimetype='application/json'
+        )
+        return response
+    response = app.response_class(
+        response=json.dumps(state_history[-1]),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/history', methods=['GET'])
+def get_history():
+    if len(state_history) == 0:
+        response = app.response_class(
+            response='We have no reported history yet',
+            status=400,
+            mimetype='application/json'
+        )
+        return response
+    response = app.response_class(
+        response=json.dumps(state_history),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'The following routes exist: ping, status, history'
 
 
 if __name__ == '__main__':
