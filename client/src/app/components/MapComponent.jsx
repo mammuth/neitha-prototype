b3de2105025dd8efea51471842826155c40b1fd2
Max Muth <kontakt@maxi-muth.de>
2017-09-09 14:18:43 +0200

    Update MapComponent

--- client/src/app/components/MapComponent.jsx
+++ client/src/app/components/MapComponent.jsx
@@ -1,47 +1,34 @@
-import {withGoogleMap, GoogleMap, Marker} from "react-google-maps";
+import React from 'react';
+import { withGoogleMap, GoogleMap } from "react-google-maps";
 
-
-// Wrap all `react-google-maps` components with `withGoogleMap` HOC
-// and name it GettingStartedGoogleMap
-const GettingStartedGoogleMap = withGoogleMap(props => (
-    <GoogleMap
-        ref={props.onMapLoad}
-        defaultZoom={3}
-        defaultCenter={{lat: -25.363882, lng: 131.044922}}
-        onClick={props.onMapClick}
-    >
-        {props.markers.map((marker, index) => (
-            <Marker
-                {...marker}
-                onRightClick={() => props.onMarkerRightClick(index)}
-            />
-        ))}
-    </GoogleMap>
+/*
+ * Sample From: https://developers.google.com/maps/documentation/javascript/examples/map-simple
+ */
+const MapGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  />
 ));
 
+/*
+ * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
+ */
 export default class Map extends React.Component {
 
-// Then, render it:
-    render(
-
-<GettingStartedGoogleMap
-containerElement={
-    <div style={{height: `100%`}}/>
-}
-mapElement={
-    <div style={{height: `100%`}}/>
-}
-onMapLoad={_.noop}
-onMapClick={_.noop}
-markers={markers}
-onMarkerRightClick={_.noop}
-/>,
-    document
-.
-    getElementById(
-
-    'root'
-)
-)
-    ;
+  render() {
+    return (
+        <div>
+            hier sollte die map sein:
+            <MapGoogleMap
+                containerElement={
+                    <div style={{height: `100%`}}/>
+                }
+                mapElement={
+                    <div style={{height: `100%`}}/>
+                }
+            />
+        </div>
+    );
+  }
 }
