29672a439e8138b7f69e3f4ae4374b4aaa35be1f
Max Muth <kontakt@maxi-muth.de>
2017-09-10 12:29:58 +0200

    Fix Status details not updating

--- client/src/app/components/StatusBarComponent.jsx
+++ client/src/app/components/StatusBarComponent.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { observer } from 'mobx-react';
 
 
+@observer
 export default class StatusBar extends React.Component {
 
 	render() {
